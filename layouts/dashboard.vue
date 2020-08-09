70898d9765c50c0fc62e2c9792aa061ade8fb879
Kirill Madorin <madorinkv@gmail.com>
2020-08-10 00:03:35 +0300

    initial commit

---
+++ layouts/dashboard.vue
@@ -0,0 +1,82 @@
+<template>
+	<v-app id="inspire">
+		<v-navigation-drawer
+			v-model="drawer"
+			app
+		>
+			<v-list dense>
+				<v-list-item link>
+					<v-list-item-action>
+						<v-icon>mdi-home</v-icon>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title>Home</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				<v-list-item link>
+					<v-list-item-action>
+						<v-icon>mdi-email</v-icon>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title>My Streams</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				<v-list-item link>
+					<v-list-item-action>
+						<v-icon>mdi-email</v-icon>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title>Settings</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				<v-list-item link>
+					<v-list-item-action>
+						<v-icon>mdi-email</v-icon>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title>Merch Store</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				<v-list-item link>
+					<v-list-item-action>
+						<v-icon>mdi-email</v-icon>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title>Payments</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+
+		<v-app-bar
+			app
+			color="indigo"
+			dark
+		>
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+			<v-toolbar-title>Watch &amp; Listen</v-toolbar-title>
+		</v-app-bar>
+
+		<v-main>
+			<Nuxt />
+		</v-main>
+		<v-footer
+			color="indigo"
+			app
+		>
+			<span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+		</v-footer>
+	</v-app>
+</template>
+
+<script>
+	export default {
+		props: {
+			source: String,
+		},
+
+		data: () => ({
+			drawer: null,
+		}),
+	}
+</script>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>Content</div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+	export default {
+		layout: 'dashboard'
+	}
+</script>
